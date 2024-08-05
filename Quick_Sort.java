41311f8310932ec9fc37f2a5b0965897e797c5e6
5id5hah <157269117+5id5hah@users.noreply.github.com>
2024-08-05 18:41:04 +0530

    Create  Quick_Sort.java

---
+++ Quick_Sort.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Quick_Sort {
+    public static void main(String[] args) {
+        int[] nums = {7,6,5,4,3,2,1,0};
+        Quick(nums,0, nums.length-1);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void Quick(int[] nums, int low, int high){
+        if(low >= high){
+            return;
+        }
+        int s = low;
+        int e = high;
+        int m = s +(e-s)/2;
+        int p = nums[m];
+
+        while (s <=e ){
+            while (nums[s] < p){
+                s++;
+            }
+            while (nums[e] > p){
+                e--;
+            }
+            if (s <= e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        Quick(nums,low,e);
+        Quick(nums,high,s);
+    }
+}
